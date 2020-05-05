95903a1da7bc4a46adc683f5dbc1ad7a97fc2593
Poom Gamer Ember <poomgamere@lightkrub.rf.gd>
2020-05-06 03:15:39 +0700

    If Else Java

---
+++ IfElse.java
@@ -0,0 +1,18 @@
+package java.for.beginner
+
+public Class IfElse{
+
+    public static void IfElse() {
+    int a = 0;
+        if (a == 0){
+            System.out.println("a = 0");
+        }
+        else if (a != 0){
+            System.out.println("a = 0");
+        }
+        else {
+            System.out.println("not found");
+        }
+    }
+    
+}
