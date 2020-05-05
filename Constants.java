bb73040ec84a96490bfd4c8c56a0972dfd9df968
Poom Gamer Ember <poomgamere@lightkrub.rf.gd>
2020-05-06 02:13:16 +0700

    Constants Java

---
+++ Constants.java
@@ -0,0 +1,14 @@
+package java.for.beginner
+
+final public class Constant { //ทำให้ไม่สามารถสืบทอดได้
+
+    public static void main (String[] args) { 
+    
+    }
+    
+    final public static void constant () { //ทำให้ Method ไม่สามารถเขียนทับได้โดย Sub Class
+    
+        final int Exam = 1; //จะต้องขึ้นต้นด้วย final เสมอและค่าจะเปลี่ยนแปลงไม่ได้
+        
+    }
+}
