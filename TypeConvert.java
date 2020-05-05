66ae8cb3aaf3550ff8ddaea9e0914b8ac87c70fb
Poom Gamer Ember <poomgamere@lightkrub.rf.gd>
2020-05-06 03:54:30 +0700

    Data Type Convert Java

--- TypeConvert.java
+++ TypeConvert.java
@@ -3,7 +3,10 @@ package java.for.beginner
 public class TypeConvert {
     
     public static void main() {
-    
+        float a = 0.5f;
+        double b = (double) a; //วิธีแปลงวิธีแรก ในที่นี้แปลงจาก float เป็น double
+        
+        String c = String.valueOf(b); //วิธีที่สอง ในที่นี้แปลง double เป็น String
     }
     
 }
