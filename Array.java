ac97b8f310dd43b07d863345cbd670748d005380
Poom Gamer Ember <poomgamere@lightkrub.rf.gd>
2020-05-06 03:48:14 +0700

    Array Java

--- Array.java
+++ Array.java
@@ -3,7 +3,16 @@ package java.for.beginner
 public class Array {
 
     public static void main() {
-        
+         int[] number = new int[5]; //สามารถเก็บได้หลายค่า
+         number[0] = 1;
+         number[1] = 2;
+         number[2] = 3;
+         number[3] = 4;
+         number[4] = 5;
+         String[] names = new String[] { "Test1", "Test2", "Test3"};
+         
+         // อ่านค่าจาก array
+         int a = number[0];
+         String Name = names[1];
     }
-    
 }
