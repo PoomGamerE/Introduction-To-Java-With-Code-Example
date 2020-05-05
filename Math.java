df447439e88637c592128584ca182b2e95d3214f
Poom Gamer Ember <poomgamere@lightkrub.rf.gd>
2020-05-06 04:04:24 +0700

    Math Calculator Java

---
+++ Math.java
@@ -0,0 +1,19 @@
+package java.for.beginner
+
+public class Math{
+    public static void main() {
+        
+    int a = 0;
+    int b = 10;
+    
+        Math.max(a, b); //ใช้หาค่าสูงสุด
+        Math.min(a, b); //ใช้หาค่าต่ำสุด
+        Math.sqrt(b); //ใช้หาค่าสแควรูท
+        Math.abs(b); //ใช้หาค่าสัมบูรณ์
+        Math.pow(b,2); //ใช้หาค่ายกกำลัง
+        Math.random(); //ใช้ในการสุ่มเลข
+        Math.ceil(); //ใช้ปัดเศษทศนิยมให้มีค่ามากขึ้น
+        Math.floor(); //ใช้ปัดเศษทศนิยมให้มีค่าน้อยลง
+        Math.round(); //ใช้ปัดเศษทศนิยมตามปกติ
+    }
+}
